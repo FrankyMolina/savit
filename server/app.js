a52202b46caf4e0b48d6c3c4be0fcabdd7368038
FrankyMolina <fjmolinatorrejon@gmail.com>
2020-03-05 17:19:44 +0100

    remove favicons to allow heroku build

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const hbs = require('hbs');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -61,7 +61,7 @@ require('./passport')(app);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 
